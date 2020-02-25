55301ab28dad78eb5e19e208ba7eb6b2f9b3af90
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-02-25 18:50:13 +0700

    tidy up

--- bidirectional.py
+++ bidirectional.py
@@ -3,59 +3,45 @@ import networkx as nx
 import time
 from queue import PriorityQueue
 
-def bi_directional_search(graph, start, goal):
-    # Check if start and goal are equal.
-    if start == goal:
-      return [start]
-    # Get dictionary of currently active vertices with their corresponding paths.
-    active_vertices_path_dict = {start: [start], goal: [goal]}
-    # Vertices we have already examined.
-    inactive_vertices = set()
+def bidirectional_search(graph, start, goal):
+  if start == goal:
+    return [start]
 
-    while len(active_vertices_path_dict) > 0:
+  active_vertices_path_dict = {start: [start], goal: [goal]}
+  inactive_vertices = set()
 
-      # Make a copy of active vertices so we can modify the original dictionary as we go.
-      active_vertices = list(active_vertices_path_dict.keys())
-      for vertex in active_vertices:
-        # Get the path to where we are.
-        current_path = active_vertices_path_dict[vertex]
-        # Record whether we started at start or goal.
-        origin = current_path[0]
-        # Check for new neighbours.
-        current_neighbours = set(graph[vertex]) - inactive_vertices
-        neighbours_list = list(current_neighbours)
+  while len(active_vertices_path_dict) > 0:
+    active_vertices = list(active_vertices_path_dict.keys())
+    for vertex in active_vertices:
+      current_path = active_vertices_path_dict[vertex]
+      origin = current_path[0]
+      current_neighbours = set(graph[vertex]) - inactive_vertices
 
-        print(neighbours_list)
+      if len(current_neighbours.intersection(active_vertices)) > 0:
+        for meeting_vertex in current_neighbours.intersection(active_vertices):
+          nx.draw_networkx_edges(graph, pos, edgelist = [(current_path[-1], meeting_vertex) for u, v, d in graph.edges(data=True)], width = 2.5, edge_color = 'r')
+          plt.pause(1)
 
-        # Check if our neighbours hit an active vertex
-        if len(current_neighbours.intersection(active_vertices)) > 0:
-          for meeting_vertex in current_neighbours.intersection(active_vertices):
-            nx.draw_networkx_edges(graph, pos, edgelist = [(current_path[-1], meeting_vertex) for u, v, d in graph.edges(data=True)], width = 2.5, edge_color = 'r')
-            plt.pause(1)
-            # Check the two paths didn't start at same place. If not, then we've got a path from start to goal.
-            if origin != active_vertices_path_dict[meeting_vertex][0]:
-              # Reverse one of the paths.
-              active_vertices_path_dict[meeting_vertex].reverse()
-              # return the combined results
-              return active_vertices_path_dict[vertex] + active_vertices_path_dict[meeting_vertex]
+          if origin != active_vertices_path_dict[meeting_vertex][0]:
+            active_vertices_path_dict[meeting_vertex].reverse()
+            return active_vertices_path_dict[vertex] + active_vertices_path_dict[meeting_vertex]
 
-        # No hits, so check for new neighbours to extend our paths.
-        if len(set(current_neighbours) - inactive_vertices - set(active_vertices))  == 0:
-          # If none, then remove the current path and record the endpoint as inactive.
-          active_vertices_path_dict.pop(vertex, None)
-          inactive_vertices.add(vertex)
+      if len(set(current_neighbours) - inactive_vertices - set(active_vertices)) == 0:
+        active_vertices_path_dict.pop(vertex, None)
+        inactive_vertices.add(vertex)
 
-        else:
-          # Otherwise extend the paths, remove the previous one and update the inactive vertices.
-          for neighbour_vertex in current_neighbours - inactive_vertices - set(active_vertices):
-            nx.draw_networkx_edges(graph, pos, edgelist = [(current_path[-1], neighbour_vertex) for u, v, d in graph.edges(data=True)], width = 2.5, edge_color = 'r')
-            plt.pause(1)
-            active_vertices_path_dict[neighbour_vertex] = current_path + [neighbour_vertex]
-            active_vertices.append(neighbour_vertex)
-          active_vertices_path_dict.pop(vertex, None)
-          inactive_vertices.add(vertex)
+      else:
+        for neighbour_vertex in current_neighbours - inactive_vertices - set(active_vertices):
+          nx.draw_networkx_edges(graph, pos, edgelist = [(current_path[-1], neighbour_vertex) for u, v, d in graph.edges(data=True)], width = 2.5, edge_color = 'r')
+          plt.pause(1)
+          
+          active_vertices_path_dict[neighbour_vertex] = current_path + [neighbour_vertex]
+          active_vertices.append(neighbour_vertex)
+        
+        active_vertices_path_dict.pop(vertex, None)
+        inactive_vertices.add(vertex)
 
-    return None
+  return -1
 
 if __name__== "__main__":
   vertices = input('Enter number of edges : ')
@@ -76,8 +62,7 @@ if __name__== "__main__":
 
   plt.show(block=False)
 
-  final_path = bi_directional_search(graph, source, goal)
-  # final_path = bfs_shortest_path(graph, source, goal)
+  final_path = bidirectional_search(graph, source, goal)
   if final_path != -1:
     for i in range (int(len(final_path)) - 1) :
       nx.draw_networkx_edges(graph, pos, edgelist = [(final_path[i], final_path[i+1]) for u, v, d in graph.edges(data=True)], width = 2.5, edge_color = 'b')
